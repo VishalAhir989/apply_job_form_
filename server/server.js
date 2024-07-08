9cfd99d512c7fddc8e2d9e0778afb7283d19e592
VishalAhir989 <157669454+VishalAhir989@users.noreply.github.com>
2024-07-08 19:24:15 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://applyjobform-rigte51j0-vishalahir989s-projects.vercel.app",
     methods: "GET,POST,PUT,DELETE,PATCH,HEAD",
     credentials: true,
 }
