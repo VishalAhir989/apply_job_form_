c061d3d25839696788744007665ab97440cdc39b
VishalAhir989 <157669454+VishalAhir989@users.noreply.github.com>
2024-07-09 18:32:09 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const router = require('./router/router');
 const connectDB = require("./db/db");
+const http = require('http');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,8 +18,16 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/", router);
 
+const server = http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://applyjobform-vishalahir989s-projects.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
+  // Your request handling logic
+});
+
 connectDB().then(() => {
-    app.listen(PORT, () => {
+    app.server(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
 }).catch((err) => {
